90b0f4e0e51a38ae39530d5007a02efee5cea650
Alex <alexjjsmith@hotmail.com>
2020-07-08 12:51:58 +0100

    Added method signatures main menu

--- src/Elementary.java
+++ src/Elementary.java
@@ -3,13 +3,45 @@ import java.io.IOException;
 public class Elementary {
     public static void main(String[] args) throws IOException {
         System.out.println("Programs\n" +
-                "0: Exit\n");
+                "0: Exit\n" +
+                "1: Hello World\n" +
+                "2: Greeting\n" +
+                "3: Greet Alice or Bob\n" +
+                "4: Sum\n" +
+                "4a: Sum O(1)\n" +
+                "5: Sum Multiples of 3 or 5\n" +
+                "6: Multiplication table to 12\n" +
+                "7: Guessing game\n");
         boolean exit = false;
         do {
             String selection = Reader.read("Please select which program you would like to run: ");
             switch (selection) {
                 case "0":
                     exit = true;
+                    break;
+                case "1":
+                    Programs.helloWorld();
+                    break;
+                case "2":
+                    Programs.greet();
+                    break;
+                case "3":
+                    Programs.greetAliceAndBob();
+                    break;
+                case "4":
+                    Programs.sum();
+                    break;
+                case "4a":
+                    Programs.sumO1();
+                    break;
+                case "5":
+                    Programs.sumMultiples3or5();
+                    break;
+                case "6":
+                    Programs.printMultiplicationTable();
+                    break;
+                case "7":
+                    Programs.guessingGame();
                     break;
                 default:
                     System.out.println("Please enter a valid program number!");
--- src/Programs.java
+++ src/Programs.java
@@ -3,18 +3,37 @@ import java.util.Random;
 
 public class Programs {
     //Write a program that prints ‘Hello World’ to the screen.
-
+    public static void helloWorld() {
+        System.out.println("Not implemented yet");
+    }
     //Write a program that asks the user for their name and greets them with their name.
-
+    public static void greet() throws IOException {
+        System.out.println("Not implemented yet");
+    }
     //Modify the previous program such that only the users Alice and Bob are greeted with their names.
-
+    public static void greetAliceAndBob() throws IOException {
+        System.out.println("Not implemented yet");
+    }
     //Write a program that asks the user for a number n and prints the sum of the numbers 1 to n
-
+    public static void sum() throws IOException {
+        System.out.println("Not implemented yet");
+    }
+    public static void sumO1() throws IOException {
+        System.out.println("Not implemented yet");
+    }
     //Modify the previous program such that only multiples of three or five are considered in the sum, e.g. 3, 5, 6, 9, 10, 12, 15 for n=17
+    public static void sumMultiples3or5() throws IOException {
+        System.out.println("Not implemented yet");
+    }
 
     //Write a program that prints a multiplication table for numbers up to 12. (lets use a while)
-
+    public static void printMultiplicationTable(){
+        System.out.println("Not implemented yet");
+    }
     /*Write a guessing game where the user has to guess a secret number. After every guess the program tells the user
       whether their number was too large or too small. At the end the number of tries needed should be printed. It
       counts only as one try if they input the same number multiple times consecutively.*/
+    public static void guessingGame() throws IOException {
+        System.out.println("Not implemented yet");
+    }
 }
